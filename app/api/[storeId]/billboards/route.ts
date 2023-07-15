74fb36109da0a3c6ee94c7ab4308249aceb1f9d9
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-07-15 12:07:33 +0530

    Fixed the issue in get all billboards api

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -50,32 +50,14 @@ export async function GET(
 ) {
   try {
     const { userId } = auth();
-    const { label, imageUrl } = await req.json();
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    if (!label) return new NextResponse("Name is required", { status: 400 });
-
-    if (!imageUrl)
-      return new NextResponse("Image URL is required", { status: 400 });
+    if (!userId) return new NextResponse("Unauthenticated", { status: 401 });
+    
     if (!params.storeId)
       return new NextResponse("Store id is required", { status: 400 });
 
-    const storeByUserId = await prismadb.store.findFirst({
+    const billboard = await prismadb.billboard.findMany({
       where: {
-        id: params.storeId,
-        userId,
-      },
-    });
-    if (!storeByUserId)
-      return new NextResponse("Unauthorized", { status: 401 });
-
-    const billboard = await prismadb.billboard.create({
-      data: {
-        label,
-        imageUrl,
         storeId: params.storeId,
       },
     });
