9a51c800b07d576bebc7c0ecf5007fca2fdb63a7
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-07-15 11:47:08 +0530

    implemented a actions for billboard

--- app/(dashboard)/[storeId]/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/billboards/[billboardId]/components/billboard-form.tsx
@@ -88,7 +88,7 @@ const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
         `/api/${params.storeId}/billboards/${params.billboardId}`
       );
       router.refresh();
-      router.push("/");
+      router.push(`/${params.storeId}/billboards`);
       toast.success("Billboard deleted.");
     } catch (error) {
       toast.error("Make sure you removed all categories using this billboard");
