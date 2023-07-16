c6c2a84a9b3bf0869a8556886b43126c730b329d
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2023-07-16 12:27:27 +0530

    implemented a frontend for size

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useRouter, useParams } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { SizeColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface SizeClientProps {
+  data: SizeColumn[];
+}
+
+export const SizeClient: React.FC<SizeClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          discrption="Manage size for your store"
+          title={`Size (${data.length})`}
+        />
+
+        <Button
+          onClick={() => router.push(`/${params.storeId}/sizes/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading title="API" discrption="API calls for Sizes" />
+
+      <ApiList entityName="sizes" entityIdName="sizeId" />
+    </>
+  );
+};
